b007af60bd872e8409bf845a71fc3aac8937d61e
xuyongcai <15089565436@163.com>
2019-05-24 16:21:24 +0800

    modify readme

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # springcloud_sell
 微服务项目
+
+### how to use
+
+1. 导入数据库doc/springcloud_sell.sql脚本，创建数据库相关数据
+
+2. 安装 rabbitmq 并启动
+
+3. 安装 redis 并启动
+
+4. 在github搭建配置中心config-repo，设置好webhooks，具体见https://github.com/xuyongcai/config-repo
+
+5. 进入项目config模块，修改配置文件，修改github地址、账号、密码、本地basedir
+
+6. 启动 eureka 服务注册中心模块
+
+7. 启动 config 配置中心模块
+
+8. 启动 其他 模块
--- order/server/src/main/java/com/imooc/order/controller/HystrixController.java
+++ order/server/src/main/java/com/imooc/order/controller/HystrixController.java
@@ -3,7 +3,6 @@ package com.imooc.order.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
